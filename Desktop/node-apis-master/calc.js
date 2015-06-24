d48f35d416c5a411b9f1bef99000edabb860341f
Michael Sankovich <misankovich@gmail.com>
2015-06-24 15:19:31 -0700

    first commit

---
+++ Desktop/node-apis-master/calc.js
@@ -0,0 +1,23 @@
+function perform(mathProb) {
+  var numRes;
+  var num1 = Number(mathProb.match(/^(\d+)/)[0]);
+  var oper = mathProb.match(/([*\+\-\/])\/?/)[1];
+  var num2 = Number(mathProb.match(/(\d+)$\/?/)[1]);
+  switch(oper){
+    case '+':
+      var numRes = num1 + num2;
+    break;
+    case '-':
+      var numRes = num1 - num2;
+    break;
+    case '*':
+      var numRes = num1 * num2;
+    break;
+    case '/':
+      var numRes = num1 / num2;
+    break;
+  }
+  return numRes;
+}
+
+module.exports = perform;
